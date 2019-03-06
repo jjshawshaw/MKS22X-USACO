74fcb39f1258d1003967d4daf7f6b71cc6ea2c63
jjshawshaw <jshaw00@stuy.edu>
2019-03-06 11:37:20 -0500

    can now read in first line

--- USACO.java
+++ USACO.java
@@ -1,10 +1,28 @@
+import java.util.*;
+import java.io.*;
 public class USACO{
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
     int[][] grid;
+    ArrayList<String> instructions = new ArrayList<String>();
+    int r;
+    int c;
+    int e;
+    int n;
+    File file = new File(filename);
+    Scanner scan = new Scanner(file);
+    r = Integer.parseInt(scan.next());
+    c = Integer.parseInt(scan.next());
+    e = Integer.parseInt(scan.next());
+    n = Integer.parseInt(scan.next());
+    System.out.println(r + ", " + c + ", " + e + ", " + n);
     return 0;
   }
 
   public static int silver(String filename){
     return 0;
+  }
+
+  public static void main(String[] args) throws FileNotFoundException{
+    System.out.println(bronze(args[0]));
   }
 }
