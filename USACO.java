85df841e456028a8df13a62f7cbaa9207f0a224a
jjshawshaw <jshaw00@stuy.edu>
2019-03-10 14:24:09 -0400

    finished silver solution

--- USACO.java
+++ USACO.java
@@ -62,8 +62,18 @@ public class USACO{
   }
 
   public static int silver(String filename) throws FileNotFoundException{
+    //stores map
     char[][] grid;
+    //stores num of possible ways to reach a square
+    int[][] pos;
+    int[][] moves = new int[][]{
+      {1, 0},
+      {-1, 0},
+      {0, 1},
+      {0, -1}
+    };
     int n, m, t, r1, r2, c1, c2;
+    //reads in file
     File file = new File(filename);
     Scanner scan = new Scanner(file);
     n = Integer.parseInt(scan.next());
@@ -76,17 +86,69 @@ public class USACO{
           grid[y][x] = line.charAt(x);
         }
       }
-    r1 = Integer.parseInt(scan.next());
-    c1 = Integer.parseInt(scan.next());
-    r2 = Integer.parseInt(scan.next());
-    c2 = Integer.parseInt(scan.next());
-    for (char[] y : grid){
-      for (char x : y){
-        System.out.print(x);
+    r1 = Integer.parseInt(scan.next()) - 1;
+    c1 = Integer.parseInt(scan.next()) - 1;
+    r2 = Integer.parseInt(scan.next()) - 1;
+    c2 = Integer.parseInt(scan.next()) - 1;
+    //sets up pos
+    pos = new int[n][m];
+    for (int y = 0; y < n; y++){
+      for (int x = 0; x < m; x++){
+        pos[y][x] = 0;
+        if (y == r1 && x == c1) pos[y][x] = 1;
+      }
+    }
+    //calculates possible ways to reach squares
+    while (t > 0){
+      //printgrid(pos, grid);
+      int[][] newpos = new int[n][m];
+      for (int y = 0; y < n; y++){
+        for (int x = 0; x < m; x++){
+          newpos[y][x] = pos[y][x];
+        }
+      }
+      for (int y = 0; y < n; y++){
+        for (int x = 0; x < m; x++){
+          if (pos[y][x] > 0){
+            for (int[] i : moves){
+              if (inbounds(y + i[0], x + i[1], n, m) &&
+                  grid[y + i[0]][x + i[1]] != '*'){
+              newpos[y + i[0]][x + i[1]] += pos[y][x];
+              }
+            }
+              boolean alone = true;
+              for (int[] i : moves){
+                if (inbounds(y + i[0], x + i[1], n, m) &&
+                    grid[y + i[0]][x + i[1]] != '*' &&
+                    pos[y + i[0]][x + i[1]] != 0){
+                alone = true;
+                }
+            }
+            if (alone) newpos[y][x] = 0;
+          }
+        }
+      }
+      pos = newpos;
+      t--;
+    }
+    return pos[r2][c2];
+  }
+
+  private static void printgrid(int[][] pos, char[][] grid){
+    for (int y = 0; y < pos.length; y++){
+      for (int x = 0; x < pos[0].length; x++){
+        String s = pos[y][x] + "";
+        if (grid[y][x] == '*') s = "*";
+        while (s.length() < 4){s += " ";}
+        System.out.print(s);
       }
       System.out.println();
     }
-    return 0;
+    System.out.println("\n");
+  }
+
+  private static boolean inbounds(int r, int c, int n, int m){
+    return (r >= 0 && c >= 0 && r < n && c < m);
   }
 
   public static void main(String[] args) throws FileNotFoundException{
